eded749e3052e38a332e513991ef27d13bb3c126
RahulJ-2211 <jesurajrahul@gmail.com>
2024-08-31 10:12:05 +0530

    Update to windows

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const myFirstElement = <h1>Hello React!</h1>
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
